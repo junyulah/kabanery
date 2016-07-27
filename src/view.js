87b5fc8658da1c596b318e90528ebed0877586a9
loveKino <842914439@qq.com>
2016-07-27 23:27:39 +0800

    bug fix

--- src/view.js
+++ src/view.js
@@ -8,23 +8,29 @@ let {
 /**
  * render function: (data) => node
  */
+
+// class level
 module.exports = (render, construct) => {
-    let node = null,
-        data = null;
-
-    let update = (path, value) => {
-        set(data, path, value);
-        // TODO diff node, opt algo
-        let newNode = render(data, ctx);
-        node.parentNode && node.parentNode.replaceChild(newNode, node);
-        node = newNode;
-    };
-
-    let ctx = {
-        update
-    };
-
+    // TODO class level API
+    // instance level
     return (obj, initor) => {
+        let node = null,
+            data = null;
+
+        let update = (path, value) => {
+            set(data, path, value);
+            // TODO diff node, opt algo
+            let newNode = render(data, ctx);
+            node.parentNode && node.parentNode.replaceChild(newNode, node);
+            node = newNode;
+        };
+
+        update.id = new Date().getTime() + '-' + Math.random();
+
+        let ctx = {
+            update
+        };
+
         data = obj;
         if (!isObject(data)) {
             throw new TypeError(`Expect object, but got ${data}. Type is ${typeof data}`);
